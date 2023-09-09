10d68e98e7967b6342857f0c04daf6bac80b1d21
Swet Singh <111109005+iamswet@users.noreply.github.com>
2023-09-10 01:48:54 +0530

    Add files via upload

---
+++ Solutions/list.cpp
@@ -0,0 +1,11 @@
+/*
+Write a CPP code to find sum of pulygon angles
+Write a CPP code to find Nth Star Number
+Write a CPP code to convert a bindary number into Octal, Hexadecimal Number
+Write a CPP code to input a date and return the day on it
+Write a CPP code to search an element in a storted and pivoted array
+Write a CPP program for block swap algorithm for array rotation
+Write a CPP code to sort elements by frequency
+Write a program to find duplicates in O(n) timme and O(1) extra space
+
+*/
