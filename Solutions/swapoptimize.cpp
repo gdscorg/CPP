e9d2428a66ff912f5dcdf79cc207c3b3bfe7c868
Swet Singh <111109005+iamswet@users.noreply.github.com>
2023-09-10 01:11:44 +0530

    Add files via upload

---
+++ Solutions/swapoptimize.cpp
@@ -0,0 +1,23 @@
+// Swap 2 nums without using a 3rd variable
+
+
+//Using XOR gate
+void swap(int *a,int *b){
+
+
+
+}
+
+//Using addition approach
+void swap(int *a,int *b){
+
+
+    
+}
+
+//Using Multiplication approach
+void swap(int *a,int *b){
+
+
+    
+}
---
+++ Solutions/swapusing.cpp
@@ -0,0 +1,8 @@
+// Swap 2 numbers using a 3rd variable
+
+void swap(int *a,int *b){
+
+
+
+
+}
